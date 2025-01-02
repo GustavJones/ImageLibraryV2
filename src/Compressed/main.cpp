d0460d23d86bcd455790608424bae528a4dd0189
GustavJones <gustavjones3@gmail.com>
2025-01-02 21:13:49 +0200

    Add support for compressed files

--- src/Compressed/main.cpp
+++ src/Compressed/main.cpp
@@ -1,3 +1,8 @@
+#include <streambuf>
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
 #include "GParsing/GParsing.hpp"
 #include "GArgs/GArgs.hpp"
 #include <cstdlib>
@@ -6,6 +11,15 @@
 #include <ios>
 #include <iostream>
 #include <vector>
+#include "Configuration/Configuration.h"
+#include "stb_image_resize2.h"
+#include "stb_image.h"
+#include "stb_image_write.h"
+#include "Compressed/Image.h"
+#include "TinyEXIF.h"
+
+constexpr static unsigned short RESOLUTION_DIVISION = 4;
+constexpr static unsigned short OUTPUT_QUILITY = 65;
 
 GParsing::HTTPRequest ReadRequest(const std::filesystem::path &_file) {
   GParsing::HTTPRequest req;
@@ -81,12 +95,24 @@ void SetupArgsParser(GArgs::Parser &_parser, int _argc, char *_argv[]) {
 }
 
 int main(int argc, char *argv[]) {
+  std::fstream f;
+  std::vector<unsigned char> fContent;
+  char fChar;
+
+  unsigned char *originalImageBuffer;
+  unsigned char *newImageBuffer;
+  int originalImageBufferX, originalImageBufferY, originalImageBufferChannels;
+  int newImageBufferX, newImageBufferY, newImageBufferChannels;
   std::vector<unsigned char> buf;
 
   GParsing::HTTPRequest req;
   GParsing::HTTPResponse resp;
   GArgs::Parser p("Image Library V2 - Reload", "V0.1");
   SetupArgsParser(p, argc, argv);
+
+  const std::filesystem::path EXECUTABLE_PATH = std::filesystem::canonical(argv[0]);
+  const std::filesystem::path EXECUTABLE_DIR = EXECUTABLE_PATH.parent_path();
+  const std::filesystem::path DATA_DIR = ((std::filesystem::path)p["filepath"][0]).parent_path();
 
   if (p.Contains("flags", "-h")) {
     p.DisplayHelp();
@@ -101,13 +127,70 @@ int main(int argc, char *argv[]) {
 
   unsigned long index = req.uri.find("/compressed");
   req.uri.erase(index, 11);
-  std::cout << req.uri << std::endl;
+
+  std::cout << "Getting Image Path" << std::endl;
+  AdvancedWebserver::Configuration c;
+  c.ReadFile(req.uri, DATA_DIR);
+
+  std::cout << "Loading Image from Path" << std::endl;
+  originalImageBuffer = stbi_load(c.GetPath().c_str(), &originalImageBufferX, &originalImageBufferY, &originalImageBufferChannels, 0);
+  newImageBufferX = originalImageBufferX / RESOLUTION_DIVISION;
+  newImageBufferY = originalImageBufferY / RESOLUTION_DIVISION;
+  newImageBufferChannels = originalImageBufferChannels;
+
+ 
+  f.open(c.GetPath(), std::ios::in | std::ios::binary);
+  TinyEXIF::EXIFInfo imageEXIF;
+  imageEXIF.parseFrom(f);
+  f.close();
+
+  newImageBuffer = new unsigned char[newImageBufferX * newImageBufferY * newImageBufferChannels]();
+  for (unsigned long i = 0; i < newImageBufferX * newImageBufferY * newImageBufferChannels; i++) {
+    newImageBuffer[i] = '\0';
+  }
+
+  std::cout << "Resize Image from Buffer" << std::endl;
+  stbir_resize_uint8_linear(originalImageBuffer, originalImageBufferX, originalImageBufferY, 0, newImageBuffer, newImageBufferX, newImageBufferY, 0, (stbir_pixel_layout)newImageBufferChannels);
+
+  ImageLibraryV2::Image img;
+  img.SetChannels(newImageBufferChannels);
+  img.SetX(newImageBufferX);
+  img.SetY(newImageBufferY);
+  img.Populate(newImageBuffer);
+
+  switch (imageEXIF.Orientation) {
+    case 8:
+      img.RotateAntiClockwise();
+      break;
+    default:
+      break;
+  }
+
+  img.ToBuffer(newImageBuffer);
+  stbi_write_jpg((p["filepath"][0] + ".jpg").c_str(), img.GetX(), img.GetY(), newImageBufferChannels, newImageBuffer, OUTPUT_QUILITY);
+  delete[] newImageBuffer;
+  stbi_image_free(originalImageBuffer);
+
+  std::cout << "Compressed: " << req.uri << std::endl;
+
+  std::cout << "Reading output file from disk" << std::endl;
+
+  
+  f.open((p["filepath"][0] + ".jpg"), std::ios::in | std::ios::binary);
+
+  while (!f.eof()) {
+    f.get(fChar);
+    fContent.push_back(fChar);
+  }
+
+  f.close();
+  std::filesystem::remove((p["filepath"][0] + ".jpg"));
 
   resp.version = "HTTP/1.1";
-  resp.response_code = 301;
-  resp.response_code_message = "Moved Permanently";
-  resp.headers.push_back({"Location", {req.uri}});
+  resp.response_code = 200;
+  resp.response_code_message = "OK";
   resp.headers.push_back({"Connection", {"close"}});
+  resp.message = fContent;
 
   buf = resp.CreateResponse();
   WriteResponse(p["filepath"][0], buf);
